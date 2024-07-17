15b870a221659d15113d8f191e6af5e9bfea0d33
yueb2 <yueb2@naver.com>
2024-07-17 13:24:39 +0900

    new file:   .eslintrc.cjs 	new file:   .gitignore 	modified:   README.md 	new file:   index.html 	new file:   package-lock.json 	new file:   package.json 	new file:   public/vite.svg 	new file:   src/App.tsx 	new file:   src/components/layout.tsx 	new file:   src/components/loading-screen.tsx 	new file:   src/main.tsx 	new file:   src/routes/create-account.tsx 	new file:   src/routes/home.tsx 	new file:   src/routes/login.tsx 	new file:   src/routes/profile.tsx 	new file:   src/vite-env.d.ts 	new file:   tsconfig.app.json 	new file:   tsconfig.json 	new file:   tsconfig.node.json 	new file:   vite.config.ts

---
+++ src/components/loading-screen.tsx
@@ -0,0 +1,18 @@
+import { styled } from "styled-components";
+
+const Wrappted = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const Text = styled.span`
+  font-size: 24px;
+`;
+export default function LoadingScreen() {
+  return (
+    <Wrappted>
+      <Text>Loading...</Text>
+    </Wrappted>
+  );
+}
